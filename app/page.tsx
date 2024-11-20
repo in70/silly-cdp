3e1581993dedffd0211e4a4a319988ce53fbc646
Ingan <ingan00001@gmail.com>
2024-11-19 23:06:36 -0800

    duplicate fix

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
             <Button />
             <div>
                 <ToggleMintRepay />
-                <MintCard />
             </div>
         </WalletProvider>
     );
