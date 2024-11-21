4b955c80774049332db0fe2f82fe81be75008815
Ingan <ingan00001@gmail.com>
2024-11-20 21:35:13 -0800

    MintCard defined but never used

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import WalletProvider from "../components/wallet-providers";
 // import { ConnectButton } from "@rainbow-me/rainbowkit";
 // import { Sidebar } from "@/components/Sidebar";
 import Button from "@/components/Button";
-import { MintCard } from "@/components/MintCard";
 import { ToggleMintRepay } from "@/components/ToggleMintRepay";
 
 export default function Home() {
